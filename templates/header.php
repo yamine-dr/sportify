8c3afd3e477f0b0d693920bc42e64990870288b3
Joumanabrm <bhm17joumana@gmail.com>
2025-04-01 11:05:38 +0200

    nouveaux header et nouvelle page avec prblm de style

---
+++ templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sportify</title>
+    <link rel="stylesheet" href="styles/bootstrap.css">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<header>
+        <nav id="nav" class="navbar navbar-expand-md bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Sportify</a>
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ms-auto">
+                        <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
+                        <a class="nav-link" href="templates/portail.php">Inscription/Connexion</a>
+                        <a class="nav-link" href="#">Ateliers</a>
+                        <a class="nav-link" href="#">Contacts</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <script src="styles/bootstrap.js"></script>
+</header>
+</html>
+
---
+++ templates/portail.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sportify</title>
+    <link rel="stylesheet" href="styles/bootstrap.css">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    
+
+    <?php include("header.php")?>
+    <main>
+        <h1>Subscribe</h1>
+    </main>
+
+    <script src="styles/bootstrap.js"></script>
+</body>
+
+</html>
