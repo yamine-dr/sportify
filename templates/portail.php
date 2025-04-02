34496c9763fcc7919e6f8d03877be270cca57bff
Yamine Daroueche <yamine.9000@gmail.com>
2025-04-02 16:46:19 +0200

    Lien portail à verification

--- templates/portail.php
+++ templates/portail.php
@@ -43,7 +43,7 @@
 					</div>
 					
 
-					<form class="mt-4 vstack gap-4">
+					<form class="mt-4 vstack gap-4" method="post" action="./verification.php">
 						<div>
 							<label class="form-label" for="email">Email</label>
 							<input type="email" class="form-control" id="email" placeholder="Votre email">
