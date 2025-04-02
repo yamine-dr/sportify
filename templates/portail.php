1112d75b1c9b5204f7034a66565dd1c05c63d098
Joumanabrm <bhm17joumana@gmail.com>
2025-04-02 16:40:13 +0200

    ajout du post

--- templates/portail.php
+++ templates/portail.php
@@ -43,7 +43,7 @@
 					</div>
 					
 
-					<form class="mt-4 vstack gap-4">
+					<form method = "POST" action = "verification.php" class="mt-4 vstack gap-4">
 						<div>
 							<label class="form-label" for="email">Email</label>
 							<input type="email" class="form-control" id="email" placeholder="Votre email">
