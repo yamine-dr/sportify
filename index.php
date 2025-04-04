6b1deb7ce91f4f84fa8590181dc98344bb5c1a07
BEKHEIRA Ryad <bekheiraryad@gmail.com>
2025-04-04 11:09:31 +0200

    quotation = page  de devis

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 require_once("src/controllers/homepage.php");
 require_once("src/controllers/auth/signin.php");
 require_once("src/controllers/auth/signup.php");
+require_once("src/controllers/quotation.php");
 
 use App\Controllers\Homepage\Homepage;
 use App\Controllers\Auth\Signin\Signin;
 use App\Controllers\Auth\Signin\Signup;
+use App\Controllers\Quotation\Quotation;
 
 try {
     if (isset($_GET["action"]) && $_GET["action"] !== "") {
@@ -14,6 +16,8 @@ try {
             (new Signin())->execute();
         } elseif ($_GET["action"] == "signup") {
             (new Signup())->execute();
+        } elseif ($_GET["action"] == "quotation") {
+            (new Quotation())->execute();
         }
     } else {
         (new Homepage())->execute();
---
+++ templates/quotation.php
@@ -0,0 +1,7 @@
+<?php $title = "Quotation"; ?>
+
+<?php ob_start(); ?>
+    <h1>Devis</h1>
+<?php $content = ob_get_clean(); ?>
+
+<?php require_once("templates/layout.php"); ?>
