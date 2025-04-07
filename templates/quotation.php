02e0814c1a41e84a505ac201e2f9d7228c8ccb4e
BEKHEIRA Ryad <bekheiraryad@gmail.com>
2025-04-07 15:54:53 +0200

    page quotation

--- templates/quotation.php
+++ templates/quotation.php
@@ -1,7 +1,43 @@
 <?php $title = "Quotation"; ?>
 
 <?php ob_start(); ?>
-    <h1>Devis</h1>
+
+<div class="container my-5">
+    <!-- Début du formulaire -->
+    <form action="traitement.php" method="POST" class="border p-4 rounded bg-light shadow-sm"> <!--envoie les données a "traitement.php" avec POST -->
+        <h1 class="mb-4">Demande de devis personnalisé</h1>
+
+        <div class="mb-3">
+            <label class="form-label">Format de la séance :</label>
+
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="type_seance" value="collectif" id="radioCollectif">
+                <label class="form-check-label" for="radioCollectif">Collectif</label>
+            </div>
+
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="type_seance" value="individuel" id="radioIndividuel">
+                <label class="form-check-label" for="radioIndividuel">Individuel</label>
+            </div>
+        </div>
+
+
+
+        <div class="mb-3">
+            <!-- Label avec classe Bootstrap -->
+            <label for="besoinsParticuliers" class="form-label">Vos besoins particuliers :</label>
+            <!-- Ajout de la classe Bootstrap pour styliser le textarea -->
+            <textarea class="form-control" name="besoins_particuliers" id="besoinsParticuliers" rows="6" placeholder="Décrivez ici vos besoins..."></textarea>
+        </div>
+
+        <div>
+            <!-- Bouton pour envoyer le formulaire -->
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </div>
+
+    </form>
+</div>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require_once("templates/layout.php"); ?>
