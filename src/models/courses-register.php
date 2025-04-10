df0297a30e3b4b6296800356e3c8efb58c584875
Joumanabrm <bhm17joumana@gmail.com>
2025-04-10 19:00:53 +0200

    evolution dans course

--- src/models/courses-register.php
+++ src/models/courses-register.php
@@ -1,5 +1,47 @@
 <?php
 
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $course = htmlspecialchars($_POST["course"]);
+    if ($courseHasLevel):   $level = htmlspecialchars($_POST["level"]);
+
+    
+    $host = 'localhost';
+    $login = 'root';
+    $dbPassword = '';
+    $dbName = 'sportify';
+
+    #CREATE DATABASE Registration
+    $codeRegistration = "
+        CREATE TABLE IF NOT EXISTS registration(
+        id INT AUTO_INCREMENT PRIMARY KEY, 
+        idClient VARCHAR(50),
+        idCourse VARCHAR(50),
+        courseLevel VARCHAR(50),
+        date DATETIME DEFAULT CURRENT_TIMESTAMP
+        )";
+    $connexion->exec($codeRegistration);
+
+    if ($courseHasLevel){
+        
+        $insertion = $connexion->prepare("INSERT INTO registration(idClient , idCourse, courseLevel) VALUES (:idClient, :idCourse, :courseLevel)");
+        $insertion->execute(['idClient' => $, 'idCourse' => $, 'courseLevel' => $]); 
+    }
+
+    
+
+    try{
+            
+        $connexion = new PDO("mysql:host=$host;dbname=$dbName", $login, $dbPassword);
+        $connexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    }
+    catch(PDOException $e){
+        echo "Erreur: ".$e->getMessage();
+    }
+}
 // add +1 user in the course selected ($_POST["course"]) in the DB
 
 header("location: ../../index.php?action=courses-register&registered=success");
